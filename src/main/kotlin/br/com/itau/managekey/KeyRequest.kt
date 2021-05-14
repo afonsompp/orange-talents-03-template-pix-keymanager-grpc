799dd8bd681e1f8187308ba3579cb0495b62a169
Afonso <afonsoprad10@gmail.com>
2021-05-14 10:24:39 -0400

    format code

--- src/main/kotlin/br/com/itau/managekey/KeyRequest.kt
+++ src/main/kotlin/br/com/itau/managekey/KeyRequest.kt
@@ -12,14 +12,14 @@ import javax.validation.constraints.Size
 @ValidKey
 @Introspected
 class KeyRequest(
-        @field:Size(max = 77) val key: String,
-        @field:NotBlank val customerId: String,
-        @field:NotNull val keyType: KeyType?,
-        @field:NotNull val accountType: AccountType?,
+	@field:Size(max = 77) val key: String,
+	@field:NotBlank val customerId: String,
+	@field:NotNull val keyType: KeyType?,
+	@field:NotNull val accountType: AccountType?,
 ) {
-    fun toKey(account: AccountResponse): Key =
-            Key(
-                    if (keyType == KeyType.RANDOM) UUID.randomUUID().toString() else key,
-                    keyType!!,
-                    account.toAccount())
+	fun toKey(account: AccountResponse): Key = Key(
+		if (keyType == KeyType.RANDOM) UUID.randomUUID().toString() else key,
+		keyType!!,
+		account.toAccount()
+	)
 }
